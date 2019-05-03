ebba3f1230f0760c1382b217fba051ce1dd30f81
yufrice <contact@yufarch.com>
2019-05-04 03:05:11 +0900

    clean

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -21,13 +21,11 @@ impl Engine {
         match ast {
             None => Ok(println!()),
             Some(ast) => {
-                //self.generator.module.print_to_stderr();
                 let module = self.generator.context.create_module("tmp");
                 let engine = module
                     .create_jit_execution_engine(OptimizationLevel::None)
                     .unwrap();
                 self.generator.jit_env(module);
-
                 self.generator.jit_eval(&ast)?;
 
                 let ret = unsafe {
@@ -36,15 +34,6 @@ impl Engine {
                         .unwrap();
                     func.call()
                 };
-                /*                 let func = self.generator.jit_eval(&ast)?;
-                let mut ret = unsafe {
-                    func.print_to_stderr();
-                    self.engine.run_function(&func, &[])
-                };
-                println!("{}", ret.as_float(&FloatType::f64_type())); */
-                //let res = self.generator.expr(&ast)?;
-                //let ref val = self.printer(&res);
-                //println!("{}", val);
                 println!("{:?}", ret);
                 Ok(())
             }
